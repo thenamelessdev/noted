f59ad126eca4c42067e4386cf1659c694cc4e6fa
thenamelessdev <nameless@thenamelessdev.com>
2026-02-20 12:24:58 +0100

    add get api endpoints

---
+++ app/api/notes/route.ts
@@ -0,0 +1,10 @@
+import getDb from "@/backend/functions/geteDb";
+import { NextResponse } from "next/server";
+
+export function GET(req: Request){
+    const db = getDb();
+    if(!db) return NextResponse.json({error: "error while getting db"}, {status: 500});
+    return NextResponse.json({
+        notes: db.notes || []
+    });
+}
--- backend/functions/geteDb.ts
+++ backend/functions/geteDb.ts
@@ -15,19 +15,19 @@ interface Note{
 
 export default function getDb(): Db | undefined{
     const dbPath = path.join(rootdir, "db.json");
-
     try{
         const db = fs.readFileSync(dbPath);
-        if(!db){
-            fs.writeFileSync(dbPath, JSON.stringify({}));
-            const createdDb = fs.readFileSync(dbPath);
-            if(!createdDb) return undefined;
-            return JSON.parse(createdDb.toString()) as Db;
-        }
         return JSON.parse(db.toString()) as Db;
     }
-    catch(err){
-        console.warn("error while reading db: ", err);
-        return undefined;
+    catch{
+        try{
+            fs.writeFileSync(dbPath, JSON.stringify({}));
+            const createdDb = fs.readFileSync(dbPath);
+            return JSON.parse(createdDb.toString()) as Db;
+        }
+        catch(err){
+            console.warn("error while getting db: ", err);
+            return undefined;
+        }
     }
 }
